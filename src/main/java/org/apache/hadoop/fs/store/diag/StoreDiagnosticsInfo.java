70d36e5a1e750fc5d68ec333db98674f20ffdb20
Steve Loughran <stevel@hortonworks.com>
2018-12-04 20:07:24 +0000

    include the spark conf vars too
    Signed-off-by: Steve Loughran <stevel@hortonworks.com>

--- src/main/java/org/apache/hadoop/fs/store/diag/StoreDiagnosticsInfo.java
+++ src/main/java/org/apache/hadoop/fs/store/diag/StoreDiagnosticsInfo.java
@@ -41,10 +41,17 @@ public class StoreDiagnosticsInfo {
   protected static final Object[][] STANDARD_ENV_VARS = {
       {"PATH", false},
       {"HADOOP_HOME", false},
+      {"HADOOP_CONF_DIR", false},
       {"HADOOP_OPTIONAL_TOOLS", false},
       {"HADOOP_SHELL_SCRIPT_DEBUG", false},
       {"HADOOP_TOOLS_HOME", false},
       {"HADOOP_TOOLS_OPTIONS", false},
+      {"HDP_VERSION", false},
+      {"SPARK_HOME", false},
+      {"SPARK_CONF_DIR", false},
+      {"PYSPARK_DRIVER_PYTHON", false},
+      {"SPARK_SCALA_VERSION", false},
+      {"YARN_CONF_DIR", false},
       
       // TODO: add the https proxy vars
   };
